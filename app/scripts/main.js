7b2c1989718ddd474b8a7112da3bef3b8153094d
Philipp Bock <p@philippbock.de>
2014-11-20 17:36:49 +0100

    Drawing flights with altitude shading, but too much performance hit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -17,34 +17,46 @@ var arrived = map.addLayer('arrived', opts);
 var underway = map.addLayer('underway', opts);
 underway.ctx.lineWidth = 3;
 arrived.ctx.lineWidth = 3;
-arrived.ctx.globalAlpha = .05;
+arrived.ctx.globalAlpha = .08;
 
 $.getJSON('data/flights.json', M.flights.pushBulk);
 M.flights.on('bulkpushed', M.clock.init);
 
 var previousArrivedLength = 0;
 var previouslyArrived = [];
+
+function drawFlight (layer) {
+	return function (flight) {
+		var alpha = layer.ctx.globalAlpha || 1;
+		var route = flight.route;
+		for (var i = 0, l = route.length; i < l-1; i++) {
+			layer.ctx.beginPath();
+			layer.moveTo(route[i]);
+			layer.lineTo(route[i+1]);
+			var opacity = route[i][2] / 45000;
+			opacity *= opacity;
+			layer.ctx.globalAlpha = opacity * alpha;
+			layer.ctx.stroke();
+		}
+		layer.ctx.globalAlpha = alpha;
+	}
+}
+
 M.clock.on('tick', function (time) {
 	var flights = M.flights.until(time);
 
 	underway.clear();
-	flights.underway.forEach(function (flight) {
-		underway.drawLine(flight.route);
-	});
+	flights.underway.forEach(drawFlight(underway));
 	if (flights.arrived.length > previouslyArrived.length) {
 		H.array.diff(
 			previouslyArrived,
 			flights.arrived,
 			function (f) { return f.object.id; }
-		).forEach(function (flight) {
-			arrived.drawLine(flight.route);
-		});
+		).forEach(drawFlight(arrived));
 	} else if (flights.arrived.length < previouslyArrived.length) {
 		// We're going back in time!
 		arrived.clear();
-		flights.arrived.forEach(function (flight) {
-			arrived.drawLine(flight.route);
-		});
+		flights.arrived.forEach(drawFlight(arrived));
 	}
 	previouslyArrived = flights.arrived;
 });
